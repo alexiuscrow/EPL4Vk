ee00da93322198807f639ea7e74658b14712307b
Oleksii Sapon <alexiusapon@gmail.com>
2015-09-13 01:46:28 +0300

    - replaced/added some fields

--- src/main/java/alexiuscrow/epl/domain/Participant.java
+++ src/main/java/alexiuscrow/epl/domain/Participant.java
@@ -1,68 +1,122 @@
 package alexiuscrow.epl.domain;
 
-import java.net.URI;
-import java.net.URL;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
 
 /**
  * Created by Alexiuscrow on 30.08.2015.
  */
 public class Participant {
-    private Long id;
-    private URI avatar;
-    private URL userPageURL;
-    private String fullName;
-    private Boolean invited;
+    private Long uid;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    private Long sex;
+    private String domain;
+    private Long city;
+    private Long country;
+    @JsonProperty("photo_50")
+    private String urlPhoto50;
+    @JsonProperty("photo_100")
+    private String urlPhoto100;
     private Boolean online;
+    @JsonProperty("can_write_private_message")
+    private Boolean canWritePrivateMsg;
+    private Boolean invited;
 
     public Participant() {}
 
-    public Participant(Long id, URI avatar, URL userPageURL, String fullName, Boolean invited, Boolean online) {
-        this.id = id;
-        this.avatar = avatar;
-        this.userPageURL = userPageURL;
-        this.fullName = fullName;
-        this.invited = invited;
+    public Participant(Long uid, String firstName, String lastName, Long sex, String domain, Long city, Long country, String urlPhoto50, String urlPhoto100, Boolean online, Boolean canWritePrivateMsg, Boolean invited) {
+        this.uid = uid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.domain = domain;
+        this.city = city;
+        this.country = country;
+        this.urlPhoto50 = urlPhoto50;
+        this.urlPhoto100 = urlPhoto100;
         this.online = online;
+        this.canWritePrivateMsg = canWritePrivateMsg;
+        this.invited = invited;
     }
 
-    public Long getId() {
-        return id;
+    public Long getUid() {
+        return uid;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setUid(Long uid) {
+        this.uid = uid;
     }
 
-    public URI getAvatar() {
-        return avatar;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setAvatar(URI avatar) {
-        this.avatar = avatar;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public URL getUserPageURL() {
-        return userPageURL;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setUserPageURL(URL userPageURL) {
-        this.userPageURL = userPageURL;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getFullName() {
-        return fullName;
+        return String.format("%s %s", getFirstName(), getLastName());
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public Long getSex() {
+        return sex;
     }
 
-    public Boolean isInvited() {
-        return invited;
+    public void setSex(Long sex) {
+        this.sex = sex;
     }
 
-    public void setInvited(Boolean invited) {
-        this.invited = invited;
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public Long getCity() {
+        return city;
+    }
+
+    public void setCity(Long city) {
+        this.city = city;
+    }
+
+    public Long getCountry() {
+        return country;
+    }
+
+    public void setCountry(Long country) {
+        this.country = country;
+    }
+
+    public String getUrlPhoto50() {
+        return urlPhoto50;
+    }
+
+    public void setUrlPhoto50(String urlPhoto50) {
+        this.urlPhoto50 = urlPhoto50;
+    }
+
+    public String getUrlPhoto100() {
+        return urlPhoto100;
+    }
+
+    public void setUrlPhoto100(String urlPhoto100) {
+        this.urlPhoto100 = urlPhoto100;
     }
 
     public Boolean isOnline() {
@@ -73,15 +127,37 @@ public class Participant {
         this.online = online;
     }
 
+    public Boolean canWritePrivateMsg() {
+        return canWritePrivateMsg;
+    }
+
+    public void setCanWritePrivateMsg(Boolean canWritePrivateMsg) {
+        this.canWritePrivateMsg = canWritePrivateMsg;
+    }
+
+    public Boolean isInvited() {
+        return invited;
+    }
+
+    public void setInvited(Boolean invited) {
+        this.invited = invited;
+    }
+
     @Override
     public String toString() {
         return "Participant{" +
-                "id=" + id +
-                ", avatar=" + avatar +
-                ", userPageURL=" + userPageURL +
-                ", fullName='" + fullName + '\'' +
-                ", invited=" + invited +
+                "uid=" + uid +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", sex=" + sex +
+                ", domain='" + domain + '\'' +
+                ", city=" + city +
+                ", country=" + country +
+                ", urlPhoto50='" + urlPhoto50 + '\'' +
+                ", urlPhoto100='" + urlPhoto100 + '\'' +
                 ", online=" + online +
+                ", canWritePrivateMsg=" + canWritePrivateMsg +
+                ", invited=" + invited +
                 '}';
     }
 }
